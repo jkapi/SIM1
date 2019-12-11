8c838566508dea5dea39b3846025a09e87422cf5
laurens423 <37153345+laurens423@users.noreply.github.com>
2019-12-11 13:06:20 +0100

    Added functions & defines
    Laatste functies toegevoegd, en defines veranderd of toegevoegd

--- Library/SIMLib.cpp
+++ Library/SIMLib.cpp
@@ -1,8 +1,16 @@
 #include "SIMLib.h"
 
 #define NOLINE 0
-#define LINE 1
+#define SENSOR01 1
+#define SENSOR02 2
+#define SENSOR03 3
+#define SENSOR04 4
+#define SENSOR05 5
 
+#define NOCOMMAND 0
+#define PAUZE 1
+#define START 2
+#define END 3
 
 SIMLib::SIMLib(){
 	
@@ -15,6 +23,21 @@ int SIMLib::readSensor(int pin){
 int SIMLib::isLine(){
 	return NOLINE;
 }
-void SIMLib::control(){
+void SIMLib::forward(){
 	
+}
+void SIMLib::turnLeft(){
+	
+}
+void SIMLib::turnRight(){
+	
+}
+void SIMLib::handleLine(){
+
+}
+int SIMLib::isCommand(){
+	return NOCOMMAND;
+}
+void SIMLib::driveParkour(){
+
 }
