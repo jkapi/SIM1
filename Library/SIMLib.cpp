75632befb4d85b257f7357562ae8e5cb8cbcd86f
laurens423 <37153345+laurens423@users.noreply.github.com>
2019-12-11 12:50:34 +0100

    Redo isLine()
    De functie isLine geeft nu een waarde terug. (NOLINE, LINE) zodat we later toe kunnen voegen door welke sensor de line wordt gezien

--- Library/SIMLib.cpp
+++ Library/SIMLib.cpp
@@ -1,16 +1,19 @@
 #include "SIMLib.h"
 
+#define NOLINE 0
+#define LINE 1
+
+
 SIMLib::SIMLib(){
 	
 }
 void SIMLib::driveMotor(int Motor, int Speed, int Action){
-
 }
 int SIMLib::readSensor(int pin){
-
+	return 0;
 }
-bool SIMLib::isLine(){
-	
+int SIMLib::isLine(){
+	return NOLINE;
 }
 void SIMLib::control(){
 	
